b4ffab117dac16fba9660f200ebe252838eb3094
Jo Imlay <jo.imlay@gmail.com>
2021-08-06 10:16:14 +0100

    updated api routes json

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,13 +2,16 @@ const getApi = (req, res, next) => {
   res.status(200).send({
     "/api": {
       "/categories": {
-        "GET /": "returns all categories"
+        "GET /": "returns all categories",
+        "POST /": "adds a category"
       },
       "/reviews": {
         "GET /": "returns all reviews",
+        "POST /": "adds a review",
         "/:review_id": {
           "GET /": "returns a single review",
           "PATCH /": "updates the vote count on a review",
+          "DELETE /": "deletes a review",
           "/comments": {
             "GET /": "returns all comments on a review",
             "POST /": "adds a comment to a review"
